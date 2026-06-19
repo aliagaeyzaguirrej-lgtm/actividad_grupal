5b90735bc40460a507ed95c47bea531b682acf8b
k1nsito <iblqckgacha@gmail.com>
2026-06-19 17:25:12 -0400

    Update promedio.py

--- promedio.py
+++ promedio.py
@@ -5,15 +5,20 @@ sistema = {
     "Adrian":{"matematicas": 70, "Datos y algoritmos": 60, "Habilidades TIC": 60}
 }
 
+#Validador de alumnos pedido en consola
 while True:
     p = input("introduce el nombre del alumno ").capitalize()
 
+    #busca si el nombre ingresado esta en el diccionario
     if p in sistema:
         print("Alumno encontrado.")
-        for materia, nota in sistema[p].items():
-            promedio = sum(sistema[p].values()) / len(sistema[p])
-
+        #recorre con un for 
+        for nota in sistema[p].items(): #recorre los items del alumno encontrado
+            promedio = sum(sistema[p].values()) / len(sistema[p]) #una variable
+            #que suma los valores del alumno y los divide por la cantidad de materias usando len
+        #impimir los resultados llamando el alumno y el promedio
         print(f"promedio de {p} es: {promedio:.2f}")
+    #repite la pregunta hasta que encuentre un alumno valido
     else:
-        print("Alumno no encontrado. Inténtalo de nuevo.")
+        print("Alumno no encontrado. Inténtalo de nuevo")
 
