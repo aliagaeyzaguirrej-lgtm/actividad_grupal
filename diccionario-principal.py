c003eddadb4833f824b3096f3410575aa139d7e2
aliagaeyzaguirrej-lgtm <aliagaeyzaguirrej@gmail.com>
2026-06-19 16:52:27 -0400

    diccionario arreglado con comentarios

--- diccionario-principal.py
+++ diccionario-principal.py
@@ -1,3 +1,4 @@
+#creamos el diccionario donde estaran los alumnos ya registrados y con sus materias con sus notas 
 print("---ALUMNOS REGISTRADOS---")
 sistema = {
     "Javier":{"matematicas":85, "Datos y algoritmos":40, "Habilidades TIC":84, "Hardware": 60, "Especificaciones y Requerimientos": 70},
@@ -7,6 +8,8 @@ sistema = {
     "Alex":  {"matematicas": 83, "Datos y algoritmos": 40, "Habilidades TIC": 20, "Hardware": 46, "Especificaciones y Requerimientos": 79},
     "Luis": {"matematicas": 96, "Datos y algoritmos": 80, "Habilidades TIC": 92, "Hardware": 68, "Especificaciones y Requerimientos": 39}
 }
+#hacemos un for donde muestre los nombres de los aluumnos y sus materias con sus notas
+#utilizamos dos variables y la función .items() para que python asigne una variable a los alumnos (los nombres de cada alumno) y notas (los mini diccionarios con las materias y notas)
 for nombre, notas in sistema.items():
     print (f"el alumno {nombre} tiene las siguientes materias y notas: {notas}")
     
